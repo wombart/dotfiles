3ed414e9b00dadefec389234b027ff9c40719864
wombart <bernhard.heinzel@gmail.com>
2014-03-29 21:53:49 +0100

    Initial commit

---
+++ zsh/lib/correction.zsh
@@ -0,0 +1,11 @@
+setopt correct_all
+
+alias man='nocorrect man'
+alias thor='nocorrect thor'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias mkdir='nocorrect mkdir'
+alias gist='nocorrect gist'
+alias heroku='nocorrect heroku'
+alias ebuild='nocorrect ebuild'
+alias hpodder='nocorrect hpodder'
